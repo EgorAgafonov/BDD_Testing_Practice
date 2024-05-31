c741a7f262b617e4bee475ffd8e829b5837a898c
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-05-31 18:31:16 +0300

    All files updated

--- features/steps/step_tutorial01.py
+++ features/steps/step_tutorial01.py
@@ -9,18 +9,27 @@ from selenium.webdriver.common.action_chains import ActionBuilder
 from selenium.webdriver import Keys
 import time
 
+
 @given(u'user is on page "{url}"')
 def step_1(context, url):
     context.browser.get(url)
-    time.sleep(3)
 
 
 @when(u'user enters "{text}" in search field')
 def step_2(context, text):
+    context.browser.implicitly_wait(5)
+    search_field = context.browser.find_element(By.CSS_SELECTOR, 'textarea[name="q"]')
+    ActionChains(context.browser).send_keys_to_element(search_field, text).pause(1).perform()
 
 
+@when('click on enter button')
+def step_3(context):
+    search_field = context.browser.find_element(By.CSS_SELECTOR, 'textarea[name="q"]')
+    ActionChains(context.browser).send_keys_to_element(search_field).send_keys(Keys.ENTER).perform()
 
 
-@then('it should have a title "Google"')
-def step(context):
-    assert context.browser.title == "Google"
+@then('page displays search results')
+def step_4(context):
+    context.browser.implicitly_wait(5)
+    results = context.browser.find_elements(By.CSS_SELECTOR, 'div[class="MjjYud"]')
+    print(f"\nПо запросу найдено: {len(results)} сайтов (01 страница).")
--- features/tutorial01_basics.feature
+++ features/tutorial01_basics.feature
@@ -1,11 +1,8 @@
+Feature: Testing google
 
-Feature: testing google
+  Scenario: Searching field tests
 
-  As a user
-  In order to find information about object
-  I want to enter object's name wright in search field
-
-Given user is on page https://www.google.ru/
-  When user enters "Авиабилеты" in search field
-   And click on enter button
-  Then page displays search results
+    Given user is on page "https://www.google.ru"
+    When user enters "Авиабилеты" in search field
+    And click on enter button
+    Then page displays search results
