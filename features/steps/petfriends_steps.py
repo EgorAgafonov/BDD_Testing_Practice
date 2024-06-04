fa33218cd15fbe8329000e85bf63e95c64317be1
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-06-04 13:32:12 +0300

    Files updated

--- features/steps/petfriends_steps.py
+++ features/steps/petfriends_steps.py
@@ -1,12 +1,7 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
-from selenium.webdriver import Keys
 from behave import *
 from locators import PetFriendsLocators
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from features.steps.settings import valid_password, valid_email
 
 
 @given('Пользователь на странице "{url}"')
@@ -15,23 +10,29 @@ def step_impl(context, url):
 
 
 @when('Пользователь вводит в поле \'Электронная почта\' свой "{email}"')
-def step_impl(context, email=os.getenv("EMAIL")):
+def step_impl(context, email=valid_email):
     context.browser.implicitly_wait(5)
-    email_field= context.browser.find_element(*PetFriendsLocators.FIELD_EMAIL)
+    email_field = context.browser.find_element(*PetFriendsLocators.FIELD_EMAIL)
     ActionChains(context.browser).send_keys_to_element(email_field, email).pause(1).perform()
 
 
 @when('вводит в поле \'Пароль\' свой "{password}"')
-def step_impl(context, password=os.getenv("PASSWORD")):
+def step_impl(context, password=valid_password):
     context.browser.implicitly_wait(5)
     pass_field = context.browser.find_element(*PetFriendsLocators.FIELD_PASSWORD)
     ActionChains(context.browser).send_keys_to_element(pass_field, password).pause(1).perform()
 
 
-@when(u'нажимает кнопку \'Войти\'')
+@when('нажимает кнопку \'Войти\'')
 def step_impl(context):
+    context.browser.implicitly_wait(5)
+    enter_btn = context.browser.find_element(*PetFriendsLocators.BUTTON_ENTER)
+    enter_btn.click()
 
 
 @then(u'система адресует Пользователя на страницу path=/all_pets')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then система адресует Пользователя на страницу path=/all_pets')
+    context.browser.implicitly_wait(5)
+    current_url = context.browser.current_url
+
+    assert current_url == '/all_pets', "\nTest Failed! Current URL's path is not /all_pets."
---
+++ features/steps/settings.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+valid_email = os.getenv("EMAIL")
+valid_password = os.getenv("PASSWORD")
