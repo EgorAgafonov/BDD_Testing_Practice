86cb135b683b16bdac5c380f612817ec0707a683
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-06-07 15:18:10 +0300

    Files updated

--- features/steps/kinopoisk_steps.py
+++ features/steps/kinopoisk_steps.py
@@ -30,3 +30,29 @@ def step_impl(context):
     results = context.browser.find_element(*KinoPoiskLocators.STACK_RESULTS).is_displayed()
 
     assert results is not False
+
+
+@when(u'Пользователь нажимает на элемент \'Расширенный поиск\'')
+def step_impl(context):
+    search_button = context.browser.find_element(*KinoPoiskLocators.SEARCH_BUTTON)
+    search_button.click()
+
+
+
+@when(u'в разделе \'Премьера\' выбирает месяц \'Июнь\'')
+def step_impl(context):
+
+
+
+@when(u'год \'2024\'')
+def step_impl(context):
+
+
+
+@when(u'страну премьеры \'США\'')
+def step_impl(context):
+
+
+
+@then(u'на странице отображается список всех премьер фильмов за указанный период')
+def step_impl(context):
